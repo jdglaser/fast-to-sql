d89f5b214a698cb67a1d2d31e4a94d4ed38b2e3c
jdglaser <jarred.dale.glaser@jci.com>
2018-04-19 10:36:23 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 10:03:10 2018
+
+@author: jglasej
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.dirname(__file__)
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+setup(
+    name='fast_to_sql',
+    version='0.1.0',
+    description='An improved way of uploading pandas DataFrames to MS SQL server',
+    long_description=long_description,
+    url='https://github.com/jdglaser/fast_to_SQL',
+    author=['Jarred Glaser'],
+    author_email='jarred.glaser@gmail.com',
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'License :: MIT License'
+        ],
+    install_requires=["pandas", "sqlalchemy", 'datetime'],
+    keywords='pandas to_sql fast',
+    packages=['fast_to_SQL'],
+)
