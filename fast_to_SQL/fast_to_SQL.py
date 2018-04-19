48ba175548c15214874f49da63fcaf944c5a9dd6
jdglaser <jarred.dale.glaser@jci.com>
2018-04-19 15:04:03 -0500

    bug fixes

--- fast_to_SQL/fast_to_SQL.py
+++ fast_to_SQL/fast_to_SQL.py
@@ -21,40 +21,57 @@ def to_sql_fast(df,name,engine,if_exists='append',series=False):
         engine.connect()
     except:
         raise InvalidEngine("Cannot connect to sqlalchemy engine.")
-        
+    
+    if series == True:
+        df = df.to_frame()
+    
+    cols = df.columns.tolist()
+    
+    # Get rid of invalid character in title
+    for i in range(len(cols)):
+        cols[i] = cols[i].replace(" ","_").replace("(","").replace(")","")
+    df.columns = cols
+    
     # Create list of columns and their datatypes
     cols = [c for c in df.columns]
     dtypes = [str(i) for i in df.dtypes]
+    print(dtypes)
     sql_dtypes = []
     for i in range(len(dtypes)):
         if dtypes[i] == 'int64':
+            df[cols[i]] = df[cols[i]].fillna(0)
             sql_dtypes.append("int")
         elif dtypes[i] == 'float64':
+            df[cols[i]] = df[cols[i]].fillna(0)
             sql_dtypes.append("float")
         elif dtypes[i] == 'object':
+            df[cols[i]] = df[cols[i]].fillna("NULL")
             sql_dtypes.append("varchar(255)")
         elif dtypes[i] == 'datetime64[ns]':
             df[cols[i]] = df[cols[i]].astype(str)
+            df[cols[i]] = df[cols[i]].fillna("NULL")
             sql_dtypes.append("datetime")
         elif dtypes[i] == 'bool':
             df.loc[df[cols[i]] == True, [cols[i]]] = 1
             df.loc[df[cols[i]] == False, [cols[i]]] = 0
+            df[cols[i]] = df[cols[i]].fillna("NULL")
             sql_dtypes.append("bit")
         else:
+            df[cols[i]] = df[cols[i]].fillna("NULL")
             sql_dtypes.append("varchar(255)")
     col_string_create = "(" + ', '.join([cols[i] + " " + sql_dtypes[i] for i in range(len(cols))]) + ")"
     col_string_insert = "(" + ', '.join(cols) + ")"
-    
+    print(df)
     # Define records for inserting
     records = [str(tuple(x)) for x in df.values]
     
     # Restructure records if a series
     if series == True:
-        records = ["(" + str(x[0]) + ")" for x in df.values]
+        records = ["('" + str(x[0]) + "')" for x in df.values]
     
     # Define insert string
     insert = "INSERT INTO {} {} VALUES ".format(name,col_string_insert)
-    
+     
     # Define the function that will insert rows
     def batch_run():
         for batch in chunk_split(records, 1000):
@@ -88,9 +105,5 @@ def to_sql_fast(df,name,engine,if_exists='append',series=False):
     # If table doesn't exist
     else:
         batch_run()
-         
-    
-    
-    
-    
-    
+
+
