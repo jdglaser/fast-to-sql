6a6040a6c4fafd0062e3a4f9c61e11897fd6c547
jareglaze <jarred.glaser@gmail.com>
2020-12-08 15:45:51 -0600

    fix __init__.py

--- fast_to_sql/__init__.py
+++ fast_to_sql/__init__.py
@@ -8,6 +8,7 @@ __author__ = "Jarred Glaser"
 __author_email__ = "jarred.glaser@gmail.com"
 __url__ = "https://github.com/jdglaser/fast-to-sql"
 __platforms__ = "ALL"
+__requires__=['pandas', 'pyodbc']
 
 __classifiers__ = [
     'Development Status :: 5 - Production/Stable',
@@ -15,8 +16,6 @@ __classifiers__ = [
     'Programming Language :: Python :: 3.8',
     'License :: OSI Approved :: MIT License'
 ]
-
-__requires__ = ["pandas", "pyodbc"]
 
 __extra_requires__ = {
 }
